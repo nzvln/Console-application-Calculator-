6c3717cc1af90c96f8753482e8bc1bb5deb2c861
Nadia <nadia@Air-Nadia.Dlink>
2021-11-12 18:55:14 +0200

    release 1

---
+++ myFirstApp/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  myFirstApp
+//
+//  Created by Nadia on 11.11.2021.
+//
+// массив содержит список доступных операций
+let availableOperations = ["*", "/", "+", "-", "^"]
+
+// запрос операции
+let operation = getOperationFromConsole()
+// запрос первого аргумента
+let firstArgument = getArgumentFromConsole(argumentNumber: 1)
+// запрос второго аргумента
+let secondArgument = getArgumentFromConsole(argumentNumber: 2)
+
+//подсчет суммы аргументов
+var result = doOpertaion(operation, with: firstArgument, and: secondArgument)
+
+//вывод результата на консоль
+print("Результат операции [\(operation)]= \(result)")
+
